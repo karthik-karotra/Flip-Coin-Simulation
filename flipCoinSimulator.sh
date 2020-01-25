9393037a293c1ee8fd6897f914c01c63178fbbea
karthik-karotra <karthikpatel54@gmail.com>
2020-01-25 13:14:50 +0530

    ADDED: Storing and calculating percentage of doublet combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,22 +1,56 @@
 #!/bin/bash -x 
 echo "Welcome to Flip coin Simulation"
-declare -A coin
 
-#variables
-headCount=0
-tailCount=0
+function flipCoin() {
+	for((i=1; i<=$1;i++))
+	do
+		flipSide=""
+		for((j=1;j<=$2;j++))
+		do
+			if [ $(( RANDOM%2 )) -eq 1 ]
+			then
+				flipSide+=H
+			else
+				flipSide+=T
+			fi
+		done
+		updateCount $flipSide
+	done
+}
 
-read -p "How many times you want to flip coin: " n
-for((i=1;i<=$n;i++))
+function updateCount() {
+	coin[$1]=$((${coin[$1]}+1))
+}
+
+function calculatePercentage() {
+	for i in ${!coin[@]}
+	do
+		coin[$i]=`echo "scale=2; ${coin[$i]}*100/$noOfFlips" | bc`
+	done
+	echo "Keys ${!coin[@]}"
+	echo "Percentage ${coin[@]}"
+}
+
+read -p "Do you want to play [y/n] : " input
+while [ $input == "y" ]
 do
-   if [ $((RANDOM%2)) -eq 1 ]
-   then
-      coin[head]=$((++headCount))
-   else
-      coin[tail]=$((++tailCount))
-   fi
+	declare -A coin
+	read -p "How many times you want to flip coin: " noOfFlips
+	read -p "Enter your choice : 1)Singlet Combination 2)Doublet Combination" choice
+	case $choice in 
+		1)
+			noOfCoins=1
+			;;
+		2)
+			noOfCoins=2
+			;;
+		*)
+			echo "Invalid Choice!!!Enter valid choice"
+			;;
+	esac
+	flipCoin $noOfFlips $noOfCoins
+	calculatePercentage
+	unset coin
+	read -p "Do you want to continue playing [y/n] : " input
 done
-echo "Keys ${!coin[@]}"
-echo "Count ${coin[@]}"
-echo "Percentage of head: `echo "scale=2; ${coin[head]}*100/$n" | bc`"%
-echo "Percentage of tail: `echo "scale=2; ${coin[tail]}*100/$n" | bc`"%
+
