953517fe6aa2438764c92ebc1cb80c544c712d76
karthik-karotra <karthikpatel54@gmail.com>
2020-01-25 11:25:48 +0530

    ADDED: Storing and calculating percentage of singlet combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 echo "Welcome to Flip coin Simulation"
+declare -A coin
 
-if [ $((RANDOM%2)) -eq 1 ]
-then
-   echo "Head"
-else
-   echo "Tail"
-fi
+#variables
+headCount=1
+tailCount=1
 
+read -p "How many times you want to flip coin: " n
+for((i=1;i<=$n;i++))
+do
+   if [ $((RANDOM%2)) -eq 1 ]
+   then
+      coin[head]=$((headCount++))
+   else
+      coin[tail]=$((tailCount++))
+   fi
+done
+echo "Keys ${!coin[@]}"
+echo "Count ${coin[@]}"
+echo "Percentage of head: `echo "scale=2; ${coin[head]}*100/$n" | bc`"%
+echo "Percentage of tail: `echo "scale=2; ${coin[tail]}*100/$n" | bc`"%
